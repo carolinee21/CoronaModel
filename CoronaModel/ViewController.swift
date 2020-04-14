a480d129d7be5df049730a8cdbfa064c3383408e
Carolinee17 <evansjcaroline@gmail.com>
2020-04-14 13:54:14 -0700

    Initial project setup

---
+++ CoronaModel/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoronaModel
+//
+//  Created by Caroline on 4/14/20.
+//  Copyright © 2020 CarolineEvans. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CoronaModelTests/CoronaModelTests.swift
@@ -0,0 +1,34 @@
+//
+//  CoronaModelTests.swift
+//  CoronaModelTests
+//
+//  Created by Caroline on 4/14/20.
+//  Copyright © 2020 CarolineEvans. All rights reserved.
+//
+
+import XCTest
+@testable import CoronaModel
+
+class CoronaModelTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
