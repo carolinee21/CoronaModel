4ceb96f62e0dc9075eb8fc5b417f03e7b60b622e
JJ Kampf <kampfj@sas.upenn.edu>
2020-05-04 00:00:20 -0500

    configure results view

--- CoronaModel/ResultsViewController.swift
+++ CoronaModel/ResultsViewController.swift
@@ -9,9 +9,34 @@
 import UIKit
 
 class ResultsViewController: UIViewController {
+    
+    var rNaught : String = ""
+    var fatalities : String = ""
+    var healthy : String = ""
+    var recovered : String = ""
+    var infected : String = ""
+    
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    @IBOutlet weak var rNaughtLabel: UILabel!
+    
+    @IBOutlet weak var healthyLabel: UILabel!
+    
+    
+    @IBOutlet weak var infectedLabel: UILabel!
+    
+    
+    @IBOutlet weak var recoveredLabel: UILabel!
+    
+    @IBOutlet weak var fatalitiesLabel: UILabel!
+    
+    override func viewDidAppear(_ animated : Bool) {
+        super.viewDidAppear(animated)
+        print("I'm getting called")
+        rNaughtLabel.text = "R0: \(rNaught)"
+        healthyLabel.text = "Healthy: \(healthy)"
+        infectedLabel.text = "Infected: \(infected)"
+        recoveredLabel.text = "Recovered: \(recovered)"
+        fatalitiesLabel.text = "Fatalities: \(fatalities)"
 
         // Do any additional setup after loading the view.
     }
